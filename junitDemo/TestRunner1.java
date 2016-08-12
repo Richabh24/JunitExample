1e95c2a138cb82f4dd17df9354cf921cbf5681e3
richa kumari <richa.kumari@tothenew.com>
2016-08-12 15:28:32 +0530

    junit examples

--- junitDemo/TestRunner1.java
+++ junitDemo/TestRunner1.java
@@ -6,8 +6,9 @@ public class TestRunner1 {
    public static void main(String[] args) {
      // Result result = JUnitCore.runClasses(ExampleEvenTest.class);
    // Result result = JUnitCore.runClasses(ExampleOddTest.class);
-      Result result = JUnitCore.runClasses(SampleJunitTest.class);
+     // Result result = JUnitCore.runClasses(SampleJunitTest.class);
      //Result result = JUnitCore.runClasses(MyoddEvenSuitTest.class);
+      Result result = JUnitCore.runClasses(AnnotationTest.class);
 
       for (Failure failure : result.getFailures()) {
          System.out.println(failure.toString());
